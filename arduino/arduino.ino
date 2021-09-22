6c045c3aef7a19fe299f1bec29825ec9ee7e4f6d
MaximilianHagelstam <maximilian.hagelstam@gmail.com>
2021-09-22 21:26:08 +0300

    Add DHT11 sensor

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,8 +1,26 @@
+#include "DHT.h"
+
+#define DHTPIN 7
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600);  
+  dht.begin();
 }
 
 void loop() {
-  Serial.println(random(0, 100));
-  delay(100);
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  Serial.print("Humidity:="); 
+  Serial.println(humidity);
+  
+  Serial.print("Temperature="); 
+  Serial.println(temperature);
+
+  Serial.println("");
+
+  delay(1000);
 }
