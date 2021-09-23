f9dd340e8dbe531a9a6854c40cec96789ef584fa
MaximilianHagelstam <maximilian.hagelstam@gmail.com>
2021-09-23 17:04:48 +0300

    Remove stuff

--- mobile/App.js
+++ mobile/App.js
@@ -1,30 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Animated } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import io from 'socket.io-client';
-import Weather from './components/Weather';
 
 export default function App() {
   const [data, setData] = useState('');
-  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const socket = io('ws://192.168.1.6:8080');
     socket.on('data', (data) => {
+      // TODO: PayloadTooLargeError: request entity too large
       setData(data);
-      setIsLoading(false);
     });
   });
 
   return (
     <View style={styles.container}>
-      {isLoading ? (
-        <View style={styles.loadingContainer}>
-          <Text style={styles.loadingText}>Fetching Temperature</Text>
-        </View>
-      ) : (
-        <Weather weather={'Drizzle'} temperature={data} />
-      )}
+      <Text>{data}</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -33,14 +26,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-  },
-  loadingContainer: {
-    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#FFFDE4',
-  },
-  loadingText: {
-    fontSize: 30,
   },
 });
