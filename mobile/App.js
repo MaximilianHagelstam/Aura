98f53382a2323c0c79b268dd9c5a2882acfc7457
MaximilianHagelstam <maximilian.hagelstam@gmail.com>
2021-09-29 19:25:55 +0300

    Update UI

--- mobile/App.js
+++ mobile/App.js
@@ -1,30 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
-import { Text } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
+import { StyleSheet, View } from 'react-native';
+import CircularProgress from 'react-native-circular-progress-indicator';
 import io from 'socket.io-client';
 
 export default function App() {
-  const [data, setData] = useState('');
+  const [data, setData] = useState();
 
   useEffect(() => {
     const socket = io('ws://192.168.1.6:8080');
     socket.on('data', (data) => {
-      setData(data);
+      setData(Number(data));
     });
   });
 
   return (
-    <LinearGradient
-      colors={['#FCA3A4', '#FD8CAD']}
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text style={{ color: 'white', fontSize: 80 }}>{data}°</Text>
+    <View style={styles.container}>
+      <CircularProgress
+        radius={150}
+        value={data}
+        maxValue={40}
+        textColor="#2F2F2F"
+        valueSuffix={'°'}
+        activeStrokeColor={'#4AA5FA'}
+        activeStrokeSecondaryColor={'#3FDFF1'}
+        inActiveStrokeColor={'#F2F3F6'}
+        inActiveStrokeWidth={10}
+        activeStrokeWidth={25}
+        duration={2000}
+      />
+
       <StatusBar style="auto" />
-    </LinearGradient>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
