2026cac6d250b963bccd459e97a380c85c21ead0
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-03-08 14:04:58 +0200

    Server ip changed

--- mobile/App.js
+++ mobile/App.js
@@ -1,15 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View } from 'react-native';
-import CircularProgress from 'react-native-circular-progress-indicator';
-import io from 'socket.io-client';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import CircularProgress from "react-native-circular-progress-indicator";
+import io from "socket.io-client";
 
 export default function App() {
   const [data, setData] = useState();
 
   useEffect(() => {
-    const socket = io('ws://192.168.1.6:8080');
-    socket.on('data', (data) => {
+    const socket = io("ws://192.168.1.2:8080");
+    socket.on("data", (data) => {
       setData(Number(data));
     });
   });
@@ -21,15 +21,14 @@ export default function App() {
         value={data}
         maxValue={40}
         textColor="#2F2F2F"
-        valueSuffix={'°'}
-        activeStrokeColor={'#4AA5FA'}
-        activeStrokeSecondaryColor={'#3FDFF1'}
-        inActiveStrokeColor={'#F2F3F6'}
+        valueSuffix={"°"}
+        activeStrokeColor={"#4AA5FA"}
+        activeStrokeSecondaryColor={"#3FDFF1"}
+        inActiveStrokeColor={"#F2F3F6"}
         inActiveStrokeWidth={10}
         activeStrokeWidth={25}
         duration={2000}
       />
-
       <StatusBar style="auto" />
     </View>
   );
@@ -38,8 +37,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
